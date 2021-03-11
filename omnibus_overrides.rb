a6f5dcd36189e887f34e50e304725032d2c0d7e4
Tim Smith <tsmith84@gmail.com>
2021-03-11 07:32:48 -0800

    Use released chef-client versions in oc-chef-pedant and chef-server-ctl
    We shouldn't be pulling in unreleased versions of chef. They have a high likelihood of being broken.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,8 +6,8 @@ override :erlang, version: "22.2"
 override :rubygems, version: "3.0.3"
 override :bundler, version: "1.17.2"  # pin to avoid double bundle error
 override :'omnibus-ctl', version: "master"
-override :chef, version: "chef-15"
-override :ohai, version: "v15.12.0"
+override :chef, version: "15.15.0"
+override :ohai, version: "15-stable"
 override :ruby, version: "2.6.6"
 override :perl, version: "5.18.1"
 
